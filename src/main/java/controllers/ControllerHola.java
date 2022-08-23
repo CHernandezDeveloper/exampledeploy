08ba8eb7512297f2be20634e7d51a43405f993b7
Camilo H <chernandez@unimayor.edu.co>
2022-08-23 16:55:29 -0500

    Initial commit

---
+++ src/main/java/controllers/ControllerHola.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerHola {
+
+    @GetMapping("/api/hola")
+    public String saludo(){
+        return "Hola, estamos en la api";
+    }
+}
